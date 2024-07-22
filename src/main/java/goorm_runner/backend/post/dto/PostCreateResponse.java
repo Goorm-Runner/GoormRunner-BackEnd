f32272ff42ac9ca7dccb89ba9e16085f9f087f44
cherry-go-round <hongseokhodev@gmail.com>
2024-07-22 16:56:40 +0900

    Refactor: getter 모든 필드 -> postId에만 적용, 생성자 lombok으로 축약

--- src/main/java/goorm_runner/backend/post/dto/PostCreateResponse.java
+++ src/main/java/goorm_runner/backend/post/dto/PostCreateResponse.java
@@ -2,21 +2,19 @@ package goorm_runner.backend.post.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@Getter
+@RequiredArgsConstructor
+@Builder
 public class PostCreateResponse {
     private final String categoryName;
-    private final Long postId;
-    private final String title;
-    private final String content;
-    private final String createdAt;
 
-    @Builder
-    public PostCreateResponse(String categoryName, Long postId, String title, String content, String createdAt) {
-        this.categoryName = categoryName;
-        this.postId = postId;
-        this.title = title;
-        this.content = content;
-        this.createdAt = createdAt;
-    }
+    @Getter
+    private final Long postId;
+
+    private final String title;
+
+    private final String content;
+
+    private final String createdAt;
 }
