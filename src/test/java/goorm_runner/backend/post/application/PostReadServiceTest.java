836fab59c62230a5e3346a9bc0e944cee16e9c99
cherry-go-round <hongseokhodev@gmail.com>
2024-07-22 17:16:01 +0900

    Test: read_failure 테스트 작성

--- src/test/java/goorm_runner/backend/post/application/PostReadServiceTest.java
+++ src/test/java/goorm_runner/backend/post/application/PostReadServiceTest.java
@@ -2,12 +2,14 @@ package goorm_runner.backend.post.application;
 
 import goorm_runner.backend.post.domain.Post;
 import goorm_runner.backend.post.dto.PostCreateRequest;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -37,5 +39,26 @@ class PostReadServiceTest {
 
         //then
         assertThat(post == findPost).isTrue();
+    }
+
+    @Test
+    void read_failure() {
+        String title = "Example title";
+        String content = "<h1>Example</h1> Insert content here.";
+        PostCreateRequest createRequest = new PostCreateRequest(title, content);
+
+        Long authorId = 1L;
+
+        String categoryName = "GENERAL";
+
+        Post post = postService.create(createRequest, authorId, categoryName);
+
+        //when
+        Long wrongId = post.getId() + 1;
+
+        //then
+        assertThatThrownBy(() -> postReadService.readPost(wrongId))
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("게시글을 찾을 수 없습니다.");
     }
 }
