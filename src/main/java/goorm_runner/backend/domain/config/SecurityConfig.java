c57210cd2d83f5ad0ce5eb66da0ba9ecb8607501
Park, minsu <cmwmss59@gmail.com>
2024-07-23 15:26:52 +0900

    feat : SecurityConfig 람다식 전환 , properties db 계정 , 비번 은닉

--- src/main/java/goorm_runner/backend/domain/config/SecurityConfig.java
+++ src/main/java/goorm_runner/backend/domain/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,8 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf
-                        .disable() // CSRF
+                .csrf(AbstractHttpConfigurer::disable // CSRF
                 )
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/auth/**").permitAll()
