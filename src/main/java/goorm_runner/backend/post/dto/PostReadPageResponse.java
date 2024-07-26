00cf2826e0d131185d0d8f0b8a430b1aa288631e
cherry-go-round <hongseokhodev@gmail.com>
2024-07-26 22:11:32 +0900

    Refactor: List<PostOverview>의 getter 불변 리스트화

--- src/main/java/goorm_runner/backend/post/dto/PostReadPageResponse.java
+++ src/main/java/goorm_runner/backend/post/dto/PostReadPageResponse.java
@@ -3,4 +3,8 @@ package goorm_runner.backend.post.dto;
 import java.util.List;
 
 public record PostReadPageResponse(List<PostOverview> overviews, ResponseMetaData responseMetaData) {
+    @Override
+    public List<PostOverview> overviews() {
+        return List.copyOf(overviews);
+    }
 }
