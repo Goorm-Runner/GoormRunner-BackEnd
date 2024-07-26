80f836c7eaf122efdb384b9013fb8df42d68cf43
cherry-go-round <hongseokhodev@gmail.com>
2024-07-26 18:50:29 +0900

    Refactor: 통합 테스트로 이름 변경

--- src/test/java/goorm_runner/backend/post/presentation/PostControllerIntegrationTest.java
+++ src/test/java/goorm_runner/backend/post/presentation/PostControllerIntegrationTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class PostControllerTest {
+class PostControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
