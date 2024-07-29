f77b9adba26d72c2c4ab1ee9de37e976eba1f32c
ByeongJun-Jang <qudwns8616@gmail.com>
2024-07-29 18:35:18 +0900

    feat : 회원 이미지 관련 DTO

---
+++ src/main/java/goorm_runner/backend/picture/dto/UpdateMemberResponseDTO.java
@@ -0,0 +1,10 @@
+package goorm_runner.backend.picture.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateMemberResponseDTO {
+    private String fileUrl;
+}
