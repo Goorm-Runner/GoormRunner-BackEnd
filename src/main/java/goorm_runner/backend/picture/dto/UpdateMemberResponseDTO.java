ad35a6f6220bd6951d96bbd154ded7ea7102ef0b
ByeongJun-Jang <qudwns8616@gmail.com>
2024-07-31 17:15:50 +0900

    Refactor : 응답 형식을 바꾸기 위해 수정

--- src/main/java/goorm_runner/backend/picture/dto/UpdateMemberResponseDTO.java
+++ src/main/java/goorm_runner/backend/picture/dto/UpdateMemberResponseDTO.java
@@ -3,8 +3,12 @@ package goorm_runner.backend.picture.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
 public class UpdateMemberResponseDTO {
-    private String fileUrl;
+    private String status;
+    private String message;
+    private Map<String, Object> data;
 }
