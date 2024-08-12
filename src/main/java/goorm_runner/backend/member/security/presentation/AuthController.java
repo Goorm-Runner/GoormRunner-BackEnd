075583abc2a8c0cca8d34e2f41c85d462d15a33e
ByeongJun-Jang <qudwns8616@gmail.com>
2024-08-12 10:41:11 +0900

    feat : delete 메서드 컨트롤러 추가

--- src/main/java/goorm_runner/backend/member/security/presentation/AuthController.java
+++ src/main/java/goorm_runner/backend/member/security/presentation/AuthController.java
@@ -1,5 +1,7 @@
 package goorm_runner.backend.member.security.presentation;
 
+import goorm_runner.backend.member.application.MemberService;
+import goorm_runner.backend.member.security.SecurityMember;
 import goorm_runner.backend.member.security.application.AuthService;
 import goorm_runner.backend.member.security.config.jwt.JwtTokenProvider;
 import goorm_runner.backend.member.security.dto.ApiResponse;
@@ -7,13 +9,12 @@ import goorm_runner.backend.member.security.dto.LoginRequest;
 import goorm_runner.backend.member.security.dto.LoginResponse;
 import goorm_runner.backend.member.security.dto.MemberSignupRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +23,7 @@ public class AuthController {
     private final AuthService authService;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
+    private final MemberService memberService;
 
     @PostMapping("/signup")
     public ResponseEntity<ApiResponse<String>> signup(@Validated @RequestBody MemberSignupRequest request) {
@@ -35,4 +37,13 @@ public class AuthController {
         return ResponseEntity.ok(new LoginResponse(token));
     }
 
+    @DeleteMapping("/drop")
+    public ResponseEntity<ApiResponse<String>> memberDrop(@AuthenticationPrincipal SecurityMember securityMember){
+
+        String username = securityMember.getUsername();
+        Long memberId = memberService.findMemberIdByUsername(username);
+
+        memberService.deleteMemberById(memberId);
+        return ResponseEntity.ok(new ApiResponse<>("회원 탈퇴 완료","204"));
+    }
 }
