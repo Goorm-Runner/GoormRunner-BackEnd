05085beb404e9ea768c9a2f8b89e810fd135ee47
ByeongJun-Jang <qudwns8616@gmail.com>
2024-08-12 20:15:13 +0900

    feat : 삭제된 것이 아닌 것을 조회하기 위해 조인 쿼리 추가

--- src/main/java/goorm_runner/backend/recruitment/domain/RecruitmentRepository.java
+++ src/main/java/goorm_runner/backend/recruitment/domain/RecruitmentRepository.java
@@ -1,9 +1,10 @@
 package goorm_runner.backend.recruitment.domain;
 
 import goorm_runner.backend.team.domain.Team;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +13,8 @@ public interface RecruitmentRepository extends JpaRepository<Recruitment, Long>,
     List<Recruitment> findByDeletedAtIsNull();
     Optional<Recruitment> findByIdAndDeletedAtIsNull(Long id);
     List<Recruitment> findByTeamAndDeletedAtIsNull(Team team);
+    List<Recruitment> findByGathering_Host_IdAndDeletedAtIsNull(Long hostId);
+
+    @Query("SELECT r FROM Recruitment r JOIN r.gathering g JOIN MemberGathering mg ON g.id = mg.gathering.id WHERE mg.guest.id = :guestId AND r.deletedAt IS NULL")
+    List<Recruitment> findByGuestIdAndDeletedAtIsNull(@Param("guestId") Long guestId);
 }
