a5c0235476b0ff8b0ddcaab842a1653edc027441
ByeongJun-Jang <qudwns8616@gmail.com>
2024-08-12 20:13:38 +0900

    feat : 선호 야구 팀 수정 기능 회원 조회

--- src/main/java/goorm_runner/backend/member/application/MemberService.java
+++ src/main/java/goorm_runner/backend/member/application/MemberService.java
@@ -4,15 +4,25 @@ import goorm_runner.backend.global.ErrorCode;
 import goorm_runner.backend.member.application.exception.MemberException;
 import goorm_runner.backend.member.domain.Member;
 import goorm_runner.backend.member.domain.MemberRepository;
+import goorm_runner.backend.member.security.dto.MemberResponse;
+import goorm_runner.backend.recruitment.domain.MemberGatheringRepository;
+import goorm_runner.backend.recruitment.domain.Recruitment;
+import goorm_runner.backend.recruitment.domain.RecruitmentRepository;
+import goorm_runner.backend.recruitment.dto.RecruitmentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final RecruitmentRepository recruitmentRepository;
+    private final MemberGatheringRepository memberGatheringRepository;
 
     public Long findMemberIdByUsername(String username) {
         Member member = memberRepository.findByLoginId(username)
@@ -28,4 +38,48 @@ public class MemberService {
         memberRepository.save(member);
     }
 
+    public void updateMemberById(Long memberId, MemberUpdateRequest request){
+        Member updateMember = memberRepository.findByIdAndDeletedAtIsNull(memberId)
+                .orElseThrow(() -> new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+
+        updateMember.update(request.getTeamId());
+        memberRepository.save(updateMember);
+    }
+
+    public MemberResponse getMemberInfo(Long memberId){
+        Member member = memberRepository.findByIdAndDeletedAtIsNull(memberId)
+                .orElseThrow(() -> new MemberException(ErrorCode.MEMBER_NOT_FOUND));
+        return new MemberResponse(member);
+    }
+
+    public List<RecruitmentResponse> findRecruitmentsByMember(Long memberId) {
+        List<RecruitmentResponse> hostMember = recruitmentRepository.findByGathering_Host_IdAndDeletedAtIsNull(memberId)
+                .stream()
+                .map(this::creativeRecruitmentResponse)
+                .toList();
+
+        List<RecruitmentResponse> guestMember = recruitmentRepository.findByGuestIdAndDeletedAtIsNull(memberId)
+                .stream()
+                .map(this::creativeRecruitmentResponse)
+                .toList();
+
+        List<RecruitmentResponse> allRecruitments = new ArrayList<>();
+        allRecruitments.addAll(hostMember);
+        allRecruitments.addAll(guestMember);
+
+        return allRecruitments;
+    }
+
+    private RecruitmentResponse creativeRecruitmentResponse(Recruitment recruitment) {
+        return new RecruitmentResponse(
+                recruitment.getId(),
+                recruitment.getTitle(),
+                recruitment.getContent(),
+                recruitment.getAddress(),
+                recruitment.getMeetTime(),
+                recruitment.getMaxParticipants(),
+                recruitment.getTeam(),
+                recruitment.getBallpark()
+        );
+    }
 }
