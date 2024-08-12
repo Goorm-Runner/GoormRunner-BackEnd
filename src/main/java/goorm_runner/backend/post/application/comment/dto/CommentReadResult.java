37460773a3c7bd3fd335c19449e0e6422f75a2cf
cherry-go-round <hongseokhodev@gmail.com>
2024-08-13 03:32:38 +0900

    Fix: updatedAt에 Content를 저장하는 버그 수정

--- src/main/java/goorm_runner/backend/post/application/comment/dto/CommentReadResult.java
+++ src/main/java/goorm_runner/backend/post/application/comment/dto/CommentReadResult.java
@@ -4,6 +4,6 @@ import goorm_runner.backend.post.domain.model.Comment;
 
 public record CommentReadResult(Long commentId, String content, String updatedAt) {
     public static CommentReadResult from(Comment comment) {
-        return new CommentReadResult(comment.getId(), comment.getContent(), comment.getContent());
+        return new CommentReadResult(comment.getId(), comment.getContent(), comment.getUpdatedAt().toString());
     }
 }
